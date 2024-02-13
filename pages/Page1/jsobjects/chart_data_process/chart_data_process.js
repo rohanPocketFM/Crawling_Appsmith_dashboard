2464af18fafda2fa8e257a73fc5295802b4fc59c
Rohan Tiwari <rohan.tiwari@pocketfm.com>
2024-02-13 12:07:13 +0000

    update

--- pages/Page1/jsobjects/chart_data_process/chart_data_process.js
+++ pages/Page1/jsobjects/chart_data_process/chart_data_process.js
@@ -1,14 +1,14 @@
 export default {
 	funcChart(){
 	let keysStartingWithV = Object.keys(Table1.triggeredRow).filter(key => /^v\d+/.test(key));
-		console.log("keysStartingWithV",keysStartingWithV)
+		// console.log("keysStartingWithV",keysStartingWithV)
 		let kv={}
 		for (const k of keysStartingWithV){
 			if (typeof Table1.triggeredRow[k]!== 'undefined' && Table1.triggeredRow[k] !== ""){
 							kv[k]=Table1.triggeredRow[k]
 			}
 		}
-		console.log(kv)
+		// console.log(kv)
 		let rawData = kv
 		const processedData = Object.keys(rawData).map(key => { 
 			const parts = key.split('_');
