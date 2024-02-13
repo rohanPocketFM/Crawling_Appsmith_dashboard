2dd8f1efe7f578893794d078e885ea9a1b8eef49
Rohan Tiwari <rohan.tiwari@pocketfm.com>
2024-02-13 11:57:31 +0000

    basic chart is done, need cosmetic changes

--- pages/Page1/jsobjects/chart_data_process/chart_data_process.js
+++ pages/Page1/jsobjects/chart_data_process/chart_data_process.js
@@ -1,12 +1,21 @@
 export default {
 	funcChart(){
 	let keysStartingWithV = Object.keys(Table1.triggeredRow).filter(key => /^v\d+/.test(key));
-		console.log(keysStartingWithV)
+		console.log("keysStartingWithV",keysStartingWithV)
 		let kv={}
 		for (const k of keysStartingWithV){
-			// console.log('k',k)
-			kv[k]=Table1.triggeredRow[k]
+			if (typeof Table1.triggeredRow[k]!== 'undefined' && Table1.triggeredRow[k] !== ""){
+							kv[k]=Table1.triggeredRow[k]
+			}
 		}
-		return kv
+		console.log(kv)
+		let rawData = kv
+		const processedData = Object.keys(rawData).map(key => { 
+			const parts = key.split('_');
+			const date = parts[1] + '-' + parts[2] + '-' + parts[3]; // YYYY-MM-DD 
+			const views = parseInt(rawData[key].replace(/,/g, ''));
+			return [ new Date(date), views ];
+		}); 
+		return processedData
 	}
 }
